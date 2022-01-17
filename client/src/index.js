bf8d324bc28c9380c297cb62743647702463531f
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-01-17 19:37:44 +0530

    updated directory

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Reactdom from "react-dom";
-import App from "../public/components/app"
+import App from "./components/app"
 Reactdom.render(
    <App />
     , document.getElementById("root")
