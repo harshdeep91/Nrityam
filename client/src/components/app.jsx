7cc902f08b01fcd30a17983110ebc330ec49b8a0
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-01-18 21:15:13 +0530

    Added caraousel

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
-
+import MCarousel from './main_caraousel/mCarousel'
 
  const App = () => {
     return (
         <div>
            <Navbar />
+           <MCarousel/>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Reactdom from "react-dom";
 import App from "./components/App"
+
 Reactdom.render(
    <App />
     , document.getElementById("root")
