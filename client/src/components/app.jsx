c55edb4b5a1bc87a98c620bc188ad3f67377d75f
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-01-19 15:51:18 +0530

    added footer

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import Marousel from './main_caraousel/mCarousel'
 import Scarousal from "./small_caraousel/scarousal"
+import Footer from './footer/Footer';
+
  const App = () => {
     return (
         <div>
@@ -10,6 +12,7 @@ import Scarousal from "./small_caraousel/scarousal"
            <Scarousal name="Trending"/>
            <Scarousal name="Top"/>
            <Scarousal name="New"/>
+           <Footer/>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Reactdom from "react-dom";
-import App from "./components/App"
+import App from "./components/app.jsx"
 
 Reactdom.render(
    <App />
