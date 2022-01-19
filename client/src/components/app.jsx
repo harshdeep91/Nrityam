42524b5e0a27dccc24d4adc2dcabfb72b4d277c8
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-01-19 15:58:20 +0530

    ok

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -12,7 +12,7 @@ import Footer from './footer/Footer';
            <Scarousal name="Trending"/>
            <Scarousal name="Top"/>
            <Scarousal name="New"/>
-           <Footer/>
+           {/* <Footer/> */}
         </div>
     )
 }
