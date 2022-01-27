e35987fe6ab829774e5168c27b1280df2d2ea89d
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-01-27 20:02:53 +0530

    sticky navbar

--- client/src/components/main_caraousel/mCarousel.jsx
+++ client/src/components/main_caraousel/mCarousel.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel'
 import "./style.css";
+import img1 from "./images/1.png";
+import img2 from "./images/2.png";
+import img3 from "./images/3.png";
 const mCarousel = () => {
   return (
     <Carousel >
       <Carousel.Item  style={{height: "50vh", width: "100vw" }}>
         <img
-          className="w-100"
-          src="https://picsum.photos/1000/1000/"
+          className="h-100 w-100"
+          src={img1}
           alt="Second slide"
         />
 
@@ -19,8 +22,8 @@ const mCarousel = () => {
       </Carousel.Item>
       <Carousel.Item style={{ height: "50vh", width: "100vw" }}>
         <img
-          className="w-100 "
-          src="https://picsum.photos/1000/1000/"
+          className="w-100 h-100"
+          src={img2}
           alt="Second slide"
         />
 
@@ -31,8 +34,8 @@ const mCarousel = () => {
       </Carousel.Item>
       <Carousel.Item  style={{ height: "50vh", width: "100vw" }}>
         <img
-          className="w-100 "
-          src="https://picsum.photos/1000/1000/"
+          className="w-100 h-100"
+          src={img3}
           alt="Second slide"
         />
 
