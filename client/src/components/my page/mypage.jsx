dbf82cde48b2eee55e55b8739de7ddde7d1f1aef
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-01-30 19:46:03 +0530

    used flex property in profile

--- client/src/components/my page/mypage.jsx
+++ client/src/components/my page/mypage.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import "./mypage.css"
 const mypage = () => {
-  return <div>
-    <div className='container'>
+  return <div className='mypage'>
+               <div className='containerz'>
+            <div className='profile'>
+        <div className='dimg'>
+        <img className='pimg'
+              src='https://image.cnbcfm.com/api/v1/image/106330923-1578676182018gettyimages-1178141599.jpeg?v=1584633147&w=1400&h=950' />
+       </div>
+        <div className='pname'>
+            HARSHDEEP RAGHUWANSHI
+        </div>
+        <div className='pfollo'>
+          <h5>Followors 39</h5>
+        </div>
+        <div className='pedit'>
+        
+          <button className="btn btn-secondary m-2">
+          <h5>Share</h5>
+          </button>
+          <button className="btn btn-secondary m-2">
+           <h5>Edit Profile</h5>
+          </button>
+          <button className="btn btn-secondary m-2">
+            <h5>Log Out</h5>
+          </button>
+
+
+
+
+        </div>
+      </div>
     </div>
     <div className='video'>
     </div>
